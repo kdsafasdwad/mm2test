b09558885c05726a3bb0446dd8caadb37a34c43b
kdsafasdwad <kenanabotafla2009@gmail.com>
2025-12-16 18:54:43 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,17 @@
-math.randomseed(os.time())
-
-while true do
-    task.wait(1)
-    print(math.random(1, 10))
+local function applyLimiter(fpsLimit, renderingEnabled)
+    game:GetService("RunService"):Set3dRenderingEnabled(renderingEnabled)
+    setfpscap(fpsLimit)
 end
+
+local function runScriptHub()
+    print("put your scripthub")
+end
+
+local function startSequence(scriptDelay, renderDelay)
+    task.wait(scriptDelay)
+    runScriptHub()
+    task.wait(renderDelay)
+    applyLimiter(15, false)
+end
+
+startSequence(5, 60) -- 5s before scripthub, 60s before rendering limit
